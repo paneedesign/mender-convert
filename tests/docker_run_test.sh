c1019e4b38c4058504e39c6c6b08d92ea4f90203
carmelo <rollsappletree@gmail.com>
2020-03-04 16:52:49 +0100

    feat: setted rasp4 as config

--- tests/docker_run_test.sh
+++ tests/docker_run_test.sh
@@ -4,8 +4,8 @@ docker run -it \
     -e INPUT_BUCKET='dev-mender-input-watchfolder' \
     -e OUTPUT_BUCKET='dev-mender-output-watchfolder' \
     -e INPUT_IMG='2019-06-20-raspbian-buster-lite.img' \
-    -e AWS_ACCESS_KEY_ID=AKIATISB4SDT7HCD7LNR \
-    -e AWS_SECRET_ACCESS_KEY=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 \
+    -e AWS_ACCESS_KEY_ID=##KEY## \
+    -e AWS_SECRET_ACCESS_KEY=##SECRET## \
     --privileged=true \
     --cap-add=SYS_MODULE \
 
